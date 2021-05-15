72d6e7e4cb5ec4e9b5a3cf4cc85601ba499a4772
kento <kento010610@icloud.com>
2021-05-15 10:53:18 +0900

    profileチャレンジ完成

--- app/src/main/java/add/murata/muraken/profile/MainActivity.kt
+++ app/src/main/java/add/murata/muraken/profile/MainActivity.kt
@@ -1,5 +1,7 @@
 package add.murata.muraken.profile
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +18,12 @@ class MainActivity : AppCompatActivity() {
             profileLabelText.text = "名前"
 
             profileCommentText.text = "Androidメンターのランディだよ"
+
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,180,220))
+
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
         button2.setOnClickListener {
             profileImage.setImageResource(R.drawable.baseball_image)
@@ -23,6 +31,12 @@ class MainActivity : AppCompatActivity() {
             profileLabelText.text = "スポーツ"
 
             profileCommentText.text = "野球観戦が好きで、スタジアムに通っている"
+
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(220,0,100))
+
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
         button3.setOnClickListener {
             profileImage.setImageResource(R.drawable.donut_image)
@@ -30,6 +44,12 @@ class MainActivity : AppCompatActivity() {
             profileLabelText.text = "好きな食べ物"
 
             profileCommentText.text = "キャンディーやドーナツが大好き"
+
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(220,0,100))
+
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
         button4.setOnClickListener {
             profileImage.setImageResource(R.drawable.gadget_image)
@@ -37,6 +57,12 @@ class MainActivity : AppCompatActivity() {
             profileLabelText.text = "趣味"
 
             profileCommentText.text = "ガジェットを集めて動かすこと"
+
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,180,120))
+
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
     }
 }
